aafbf42185a5a53234419d64b3cc6d2f3f7a534e
Tanuja Patel <tanujapatel@Tanujas-MacBook-Pro.local>
2024-07-27 23:11:16 +0530

    updated to remove duplications

--- tests/steps/login.steps.js
+++ tests/steps/login.steps.js
@@ -3,30 +3,9 @@ const { Given, When, Then } = require('@wdio/cucumber-framework');
 const { expect } = require('chai');
 // let platformname = process.env.platformname;
 
-// const login = {
-// 	action: require('../../../../resources/pages/login/action.js'),
-// 	assert: require('../../../../resources/pages/login/assert.js')
-// };
-// const mainPage = {
-// 	assert: require('../../../../resources/pages/main-page/assert.js')
-// };
 const assertDet = require('../../resources/pages/assert.js')
 const login = require('../../resources/pages/login.page.js')
-// const mainPage = require('../../resources/pages/main.page.js')
 const variable = require('../../resources/testdata/variable.js');
-
-
-// Given(
-// 	/^Sauce Demo app opened and directed to login page$/,
-// 	async function () {
-// 		await login.accessLoginPage();
-// 		const response = await login.checkContent();
-
-// 		expect(response.pageTitle).equal(assertDet.attribute.LOGIN.pageTitleText, response.pageTitle);
-// 		expect(response.pageDesc).equal(assertDet.attribute.LOGIN.pageDescText, response.pageDesc);
-// 		expect(response.loginBtn).equal(assertDet.attribute.LOGIN.loginBtnText, response.loginBtn);
-// 	}
-// );
 
 When(
 	/^I enter "([^"]*)?" user deatils on login fields$/,
